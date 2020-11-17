15e2d80ed0766d172b13f9cbb2124f027f972d75
rrokowski <74591317+rrokowski@users.noreply.github.com>
2020-11-17 09:35:54 +0100

    Add files via upload

---
+++ lista.php
@@ -0,0 +1,15 @@
+<?php
+    require_once 'vendor/autoload.php';
+
+    $faker = Faker\Factory::create();
+    
+    $myObj = array();
+    for ($i = 0; $i < 50; $i++) {
+        $myObj[] = ['id'=>$faker->unique()->numberBetween(1,1000), 'name'=>$faker->name, 'email'=>$faker->email];         
+    };
+
+    $myJSON = json_encode($myObj);
+    echo($myJSON);
+    die();
+    //file_put_contents('lista.json', $myJSON);
+?>
---
+++ pagingJson.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>pagingJSON</title>
+        <meta name="description" content="#"/>
+        <meta name="keywords" content="#"/>
+        
+        <link rel='stylesheet' href='https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css'><link rel="stylesheet" href="./style.css">
+        
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Faker/3.1.0/faker.min.js" type="text/javascript"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.21/js/jquery.dataTables.min.js'></script>
+        <script type="text/javascript" src="lista.json"></script>
+        
+    </head>
+    
+    <body>
+        <div id="container">
+            <table id="table">
+                <thead>
+                    <tr>
+                        <th>ID</th> <th>Name</th> <th>e-mail</th>
+                    </tr>
+                </thead>
+            
+                <tbody>
+                </tbody>
+                
+            </table>
+        </div>
+        
+        <script>
+            
+        $(document).ready(function loadUsers() {
+            let xhr = new XMLHttpRequest();
+
+            // OPEN - call type, url/file, sync/async
+            xhr.open('GET', "lista.php", true);
+
+            //SEND
+            xhr.send(); 
+
+            xhr.onload = function(){
+                let users = JSON.parse(this.response);
+                console.log(users);
+                
+                for (let i = 0; i < users.length; i++) {
+                    $('#table').append('<tr><td>'+users[i].id+'</td><td>'+users[i].name+'</td><td>'+users[i].email+'</td></tr>'); 
+                };
+                
+                $('#table').DataTable( {
+                    "lengthChange": false,
+                });
+            };
+        });    
+        </script>
+    </body>
+</html>
