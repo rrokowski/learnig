47533923cdd57063b087d4b92cdbdbbf678b1f3a
rrokowski <rrokowski@gmail.com>
2020-12-03 15:14:33 +0100

    poprawiona ścieżka absolutna na relatywną

--- generator.php
+++ generator.php
@@ -1,37 +1,38 @@
 <?php
-//generowanie listy fakerem
-require_once 'vendor/autoload.php';
+    //generowanie listy fakerem
+    require_once 'vendor/autoload.php';
 
-$faker = Faker\Factory::create();
+    $faker = Faker\Factory::create();
+    
+    $myObj = array();
+    for ($i = 0; $i < 50; $i++) {
+        $myObj[] = ['name'=>$faker->name, 'email'=>$faker->email];         
+    };
+    
+    //połączenie z mysql
+    $host = 'localhost';
+    $user = 'root';
+    $pass = 'mysql';
+    $db = 'lista';
+    $conn = mysqli_connect($host, $user, $pass, $db);
+    if (!$conn) {
+        die ("bląd połączenia" .mysqli_connect_error());
+    }
 
-$myObj = array();
-for ($i = 0; $i < 50; $i++) {
-    $myObj[] = ['name' => $faker->name, 'email' => $faker->email];
-};
+    //czyszczenie tabeli users
+    $removeTable = "DELETE FROM users";
+    mysqli_query($conn, $removeTable);
 
-//połączenie z mysql
-$host = 'localhost';
-$user = 'root';
-$pass = 'mysql';
-$db = 'lista';
-$conn = mysqli_connect($host, $user, $pass, $db);
-if (!$conn) {
-    die ("bląd połączenia" . mysqli_connect_error());
-}
-
-//czyszczenie tabeli users
-$removeTable = "DELETE FROM users";
-mysqli_query($conn, $removeTable);
-
-//wgrywanie danych do tabeli users
-if (is_array($myObj)) {
-    foreach ($myObj as $row => $value) {
+    //wgrywanie danych do tabeli users
+    if (is_array($myObj)) {
+        foreach ($myObj as $row => $value) {
         $name = mysqli_real_escape_string($conn, $value["name"]);
         $email = mysqli_real_escape_string($conn, $value["email"]);
-
-        $sql = "INSERT INTO users(name, email) VALUES ('$name', '$email')";
-        mysqli_query($conn, $sql);
+        
+        $sql= "INSERT INTO users(name, email) VALUES ('$name', '$email')";
+        mysqli_query($conn, $sql);    
+        }   
     }
-}
-
-mysqli_close($conn);
+    
+    mysqli_close ($conn);
+?>
