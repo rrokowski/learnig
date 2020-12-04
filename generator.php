a137f1b309465c02a3e56f1dfc77ad631ccab07a
tomas.perunsky <tomas.perunsky@gmail.com>
2020-12-04 11:24:24 +0100

    Ponownie dodane sciezki relatywne i poprawione formatowanie

--- generator.php
+++ generator.php
@@ -1,38 +1,37 @@
 <?php
-    //generowanie listy fakerem
-    require_once '/Applications/AMPPS/www/pagingJSON/vendor/autoload.php';
+//generowanie listy fakerem
+require_once 'vendor/autoload.php';
 
-    $faker = Faker\Factory::create();
-    
-    $myObj = array();
-    for ($i = 0; $i < 50; $i++) {
-        $myObj[] = ['name'=>$faker->name, 'email'=>$faker->email];         
-    };
-    
-    //połączenie z mysql
-    $host = 'localhost';
-    $user = 'root';
-    $pass = 'mysql';
-    $db = 'lista';
-    $conn = mysqli_connect($host, $user, $pass, $db);
-    if (!$conn) {
-        die ("bląd połączenia" .mysqli_connect_error());
-    }
+$faker = Faker\Factory::create();
 
-    //czyszczenie tabeli users
-    $removeTable = "DELETE FROM users";
-    mysqli_query($conn, $removeTable);
+$myObj = array();
+for ($i = 0; $i < 50; $i++) {
+    $myObj[] = ['name' => $faker->name, 'email' => $faker->email];
+};
 
-    //wgrywanie danych do tabeli users
-    if (is_array($myObj)) {
-        foreach ($myObj as $row => $value) {
+//połączenie z mysql
+$host = 'localhost';
+$user = 'root';
+$pass = 'mysql';
+$db = 'lista';
+$conn = mysqli_connect($host, $user, $pass, $db);
+if (!$conn) {
+    die ("bląd połączenia" . mysqli_connect_error());
+}
+
+//czyszczenie tabeli users
+$removeTable = "DELETE FROM users";
+mysqli_query($conn, $removeTable);
+
+//wgrywanie danych do tabeli users
+if (is_array($myObj)) {
+    foreach ($myObj as $row => $value) {
         $name = mysqli_real_escape_string($conn, $value["name"]);
         $email = mysqli_real_escape_string($conn, $value["email"]);
-        
-        $sql= "INSERT INTO users(name, email) VALUES ('$name', '$email')";
-        mysqli_query($conn, $sql);    
-        }   
+
+        $sql = "INSERT INTO users(name, email) VALUES ('$name', '$email')";
+        mysqli_query($conn, $sql);
     }
-    
-    mysqli_close ($conn);
-?>
+}
+
+mysqli_close($conn);
--- lista.php
+++ lista.php
@@ -1,24 +1,22 @@
 <?php
-    //połączenie z mysql
-    $host = 'localhost';
-    $user = 'root';
-    $pass = 'mysql';
-    $db = 'lista';
-    $conn = mysqli_connect($host, $user, $pass, $db);
-    if (!$conn) {
-        die ("bląd połączenia" .mysqli_connect_error());
-    }
+//połączenie z mysql
+$host = 'localhost';
+$user = 'root';
+$pass = 'mysql';
+$db = 'lista';
+$conn = mysqli_connect($host, $user, $pass, $db);
+if (!$conn) {
+    die ("bląd połączenia" . mysqli_connect_error());
+}
 
-    //pobranie danych z tabeli users
-    $sql = mysqli_query($conn, "SELECT * FROM users");
-    $myObj = array();
-    
-    while ($rowUser = mysqli_fetch_assoc($sql)) 
-         $myObj[] = $rowUser;
-         //print_r($myObj);
-    
-    //zapisanie danych jako json     
-    $myJSON = json_encode($myObj);
-    echo($myJSON);
-    die();
-?>
+//pobranie danych z tabeli users
+$sql = mysqli_query($conn, "SELECT * FROM users");
+$myObj = array();
+
+while ($rowUser = mysqli_fetch_assoc($sql)) {
+    $myObj[] = $rowUser;
+}
+//print_r($myObj);
+
+//zapisanie danych jako json
+die(json_encode($myObj));
