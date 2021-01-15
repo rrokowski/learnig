b24adebe0bdcf7b91f64958ef16c438f7664ce82
rrokowski <rrokowski@gmail.com>
2021-01-15 13:49:16 +0100

    wprowadziłem kolejne zmiany, ale nie działa

--- api11.php
+++ api11.php
@@ -6,8 +6,8 @@
         $url = "http://api.coindesk.com/v1/bpi/historical/close.json?start=".$startDate."&end=".$endDate;
      //   $url1 = "http://api.coindesk.com/v1/bpi/currentprice.json";
         $json = file_get_contents($url);
-        $myJSON = json_encode($json);
-        echo($myJSON);
+        //$myJSON = json_encode($json);
+        //echo($myJSON);
         //$data = json_decode($json, true);
         //print_r($data);
         
