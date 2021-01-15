c3835f2cd3196d2944c7abad5fb530d0acdd1004
rrokowski <rrokowski@gmail.com>
2021-01-15 13:58:45 +0100

    działa!

--- api11.php
+++ api11.php
@@ -4,15 +4,9 @@
         $endDate = $_POST['endDate'];
        
         $url = "http://api.coindesk.com/v1/bpi/historical/close.json?start=".$startDate."&end=".$endDate;
-     //   $url1 = "http://api.coindesk.com/v1/bpi/currentprice.json";
+        //$url1 = "http://api.coindesk.com/v1/bpi/currentprice.json";
         $json = file_get_contents($url);
-        //$myJSON = json_encode($json);
-        //echo($myJSON);
-        //$data = json_decode($json, true);
-        //print_r($data);
-        
-        //$file = file_put_contents("data1.json", $json);
-
+        echo($json);
         die();
 
         
